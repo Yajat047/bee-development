72f1578d2fbfc2e4fd66cb8d9f8408138af10cc4
Yajat Dureja <75976299+Yajat047@users.noreply.github.com>
2024-12-10 18:13:54 +0000

    manual url

--- client/src/components/admin/dashboardAdmin/TodaySell.js
+++ client/src/components/admin/dashboardAdmin/TodaySell.js
@@ -4,7 +4,7 @@ import moment from "moment";
 import { DashboardContext } from "./";
 import { todayAllOrders } from "./Action";
 
-const apiURL = process.env.REACT_APP_API_URL;
+const apiURL = "https://bee-development-production.up.railway.app";
 
 const SellTable = () => {
   const history = useHistory();
